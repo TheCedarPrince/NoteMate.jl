acc5a5bbba6439e67470a0d03895bc62f1510830
Documenter.jl <documenter@juliadocs.github.io>
2023-05-23 19:15:07 +0000

    build based on 5f7803d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Welcome-to-the-NoteMate.jl-Docs!","page":"Home","title":"Welcome to the NoteMate.jl Docs! 👋","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The perfect companion for working with your notes ","category":"page"}]
+}
