3cf6b6aec5609202a18d6f7ad270e9759989584c
Sevoris Doe <74614227+SevorisDoe@users.noreply.github.com>
2023-06-15 22:09:32 +0200

    Added docstrings to rest of franklin.jil
    Rolled the improved docstrings by @TheCedarPrince into the existing docstrings, and added docstrings of the same style to the rest of the methods. 
    
    Docstring for generate_citation() still requires a clarification for the citations keyword argument - structure of the agument data unclear.

--- src/targets/franklin.jl
+++ src/targets/franklin.jl
@@ -1,10 +1,19 @@
 """
-    create_franklin_note(note::Note, date_format::String="U d y")
+    create_franklin_note(note::Note; date_format::String = "U d y")
 
-Transform a generic NoteMate Note into a [FranklinNote](@ref) data structure with special metadata from note content.
+Transform a generic `Note` into a `[FranklinNote](@ref)` data structure with special metadata from `Note` content.
 
-FranklinNote require special information used to configure properties like RSS metadata. This information can be 
-derived from the information in a generic NoteMate note structure. 
+# Arguments 
+
+- `note`: a `Note` object that will be used for conversion
+
+# Keyword Arguments 
+
+- `date_format`: a `String` that accepts a date format; default "U d y" (see: `Dates.format` for options)
+
+# Return 
+
+- A newly prepared `FranklinNote` object
 """
 function create_franklin_note(note::Note; date_format::String="U d y")
     title = note.title
@@ -42,16 +51,40 @@ end
 """
     generate_franklin_template()
 
-Translate submitted information into a multi-line string of Fraklin `+++` [page variable declarations](https://franklinjl.org/syntax/page-variables/).
+Generates a template for a Franklin markdown page.
 
-Fraklin page variables may be declared as Julia expressions between a leading and trailing `+++`. This function turns
-what information is submitted into individual line statements. It will return at least an empty page variable declaration
-of the form 
+# Keyword Arguments
+
+- `title`: Page title. Default is `nothing`
+- `slug`: Specifies web page url path (after base path). Default is `nothing` 
+- `tags`: Keywords or tags associated with page. Default is `nothing` 
+- `description`: Description of page contents. Default is `nothing` 
+- `rss_title`: Page title that shows up on RSS feeds. Default is `nothing` 
+- `rss_description`: Description that goes along with RSS updates. Default is `nothing` 
+- `rss_pubdate`: Publication date for RSS feed. Default is `nothing`
+
+These kwargs are "page variables" that come directly from `Franklin.jl`'s documentation.
+For more specific details, please see [Page Variables](https://franklinjl.org/syntax/page-variables/) 
+
+# Return
+
+- A `String` object that contains a valid Franklin template that can then be modified further
+
+# Example 
+
+If the following call is made: `generate_franklin_template(; title = "All-Payer Claims Database", tags = ["apcd", "claims", "database"])`, the following string will be returned:
 ```
 +++
+
+title = "All-Payer Claims Database"
+
+tags = ["zettel", "apcd", "claims", "database", "archive"]
+
 +++
+
 ```
-if no arguments are submitted on the call. 
+This can then be edited or formatted further as needed.
+
 """
 function generate_franklin_template(; title=nothing, slug=nothing, tags=nothing, description=nothing, rss_title=nothing, rss_description=nothing, rss_pubdate=nothing)
 
@@ -91,9 +124,13 @@ function generate_franklin_template(; title=nothing, slug=nothing, tags=nothing,
 end
 
 """
-    generate_comments
+    generate_comments()
 
-Generates a Fraklin page macro string used to embedd a comments section utility to a page.
+Generates a Fraklin page macro to embed a comments section into a page.
+
+# Return 
+
+- A string containing a Franklin macro to add comments to a Franklin page. See [Franklin Utils](https://franklinjl.org/syntax/utils/) for more information.
 """
 function generate_comments()
     """
@@ -104,9 +141,13 @@ function generate_comments()
 end
 
 """
-    generate_table_of_contents
+    generate_table_of_contents()
 
-Format a "Table of contents" heading and a hyperlinked table  of contents under that heading.  
+Generate a hyperlinked *Table of Contents* for a Franklin page.
+
+# Return 
+
+- A string containing a Franklin macro to add a table of contents to a Franklin page. See [Table of Contents](https://franklinjl.org/syntax/markdown/index.html#table_of_contents) for more information.
 """
 function generate_table_of_contents()
     """
@@ -115,14 +156,45 @@ function generate_table_of_contents()
 end
 
 """
-TODO: ADD DOCS
+    generate_bibliography
+
+Generate a bibliography section for a Franklin page. 
+
+# Arguments
+
+- `note`: a `FranklinNote` struct whose bibliography string will be included in the output
+
+# Return 
+- a string starting with a Fraklin section header, followed after a double newline by the bibliography string stored in the note struct. 
+
 """
 function generate_bibliography(note::FranklinNote)
     "\nBibliography\n==========\n\n" * note.bibliography
 end
 
 """
-TODO: ADD DOCS
+    generate_note_summary
+
+Generate a note summary section using note title, publishing date, rss summary and any keywords.
+
+# Arguments
+
+- `note`: a `FranklinNote` struct whose information is formated for the note summary section
+
+# Return
+- a string formated according to the Open Knowledge Model standard for Franklin, starting with a note title, followed by the Date in `monthname day year` form, the RSS summary of the note, and comma-separated keywords for the note.
+An example formating: 
+```
+Example title:
+=========
+
+**Date:** May 12 2020
+
+**Summary:** This is the summary of an example note
+
+**Keywords:** example files, demonstration, documentation
+```
+
 """
 function generate_note_summary(note::FranklinNote)
     date = Date(note.rss_pubdate...)
@@ -134,18 +206,48 @@ function generate_note_summary(note::FranklinNote)
     **Date:** $(monthname(date)) $(day(date)) $(year(date))
 
     **Summary:** $(note.rss_description)
-
+    
     **Keywords:** $(note.keywords)
     """
 end
 
 """
-TODO: ADD DOCS
+    generate_references
+
+Returns the references string for the note. 
+
+# Arguments
+
+- `note`: a `FranklinNote` struct whose references string will be included in the output
+
+# Return 
+- the note references string as is, without further formating
+
 """
 function generate_references(note::FranklinNote)
     note.references
 end
 
+"""
+    generate_citation()
+
+Generate the citation string that can be used to reference the page of this document. 
+
+# Arguments
+
+- `note`: a `FranklinNote` struct whose information will be used to format the citation strings
+
+# Keyword Arguments
+- `citations` : TODO clarify type and structure here 
+
+# Return
+Return string always begin with `## How to Cite\\n\\n` followed by either:
+- if the citation argument is empty, returns a standard citation string with primary author name, note title, homepage link and `monthname day year` date.
+- if the citation argument is non-empty, will generate a longer citation string with list of all authors before note title, homepage link and `monthname day year` date.
+
+
+FIXME: This is currently hardcoded for my own personal website. We need to adjust this to not be that way.
+"""
 function generate_citation(note::FranklinNote; citations="")
 
     date = Date(note.rss_pubdate...)
